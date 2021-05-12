8d1786d37ade489bdf7f172ff11fbd66d79f7f0e
Sahas Punchihewa <sahasp@techprovint.com>
2021-05-12 13:03:47 +0530

    created header class

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+class Header extends Component
+{
+    render()
+    {
+        return(
+            <header>
+                <div>Logo</div>
+                <input type="Text"/>
+            </header>
+        )
+    }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Header from './components/header';
+
 const App=()=>
 {
   return(
-    <div className="newElement">
-      <h1>Hello World!</h1>
-      <div>Hey</div>
+    <div>
+      <Header/>
     </div>
   )
 }
