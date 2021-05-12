e92b37da1b58768535acad5aeab38f1d42574ebd
Sahas Punchihewa <sahasp@techprovint.com>
2021-05-12 13:14:33 +0530

    added inline css

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,18 @@ class Header extends Component
 {
     render()
     {
+        const styles={
+            header:{
+                background:'#78ccff',
+                textAlign:'center'
+            },
+            logo:{
+                color:'red'
+            }
+        }
         return(
-            <header>
-                <div>Logo</div>
+            <header style={styles.header}>
+                <div style={styles.logo}>Logo</div>
                 <input type="Text"/>
             </header>
         )
